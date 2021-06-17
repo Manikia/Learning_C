f95a7fd9299346ba0a7962fae81caa5ee8edb6c6
Monika <monikaangel.ma15@gmail.com>
2021-06-16 18:27:29 -0700

    learning about size in array from user

---
+++ FlexibleArray.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    //we can make empty brackets
+    //we create flexible arrays by declaring empty brackets for an array
+
+    
+
+    return 0;
+}
--- variableArrayChallenge.c
+++ variableArrayChallenge.c
@@ -4,35 +4,25 @@
 
 int main(void)
 {
-    int size;
+    int size = 0, i = 0, element = 0;
 
     printf("How many elements do you want to sum?");
-    scanf(" %d", &size);
-
-    int array[size];
-
-    int i = 0;
-    int element = 0;
-    int *pntr;
-    *pntr = array[i];
+    scanf("%d", &size);
+    int array[size]; //makes the size of array as given from user
 
     printf("Insert your elements: ");
     for (i = 0; i < size; i++)
     {
-        scanf(" %d", &element);
-        element += element;
-
-        array[i];
+        scanf("%d", &array[i]); //puts user input into array indexes
+        element = element + array[i]; //adds up array elements
     }
 
     printf("You inputted: ");
     for (i = 0; i < size; i++)
     {
-        printf(pntr);
-        pntr++;
+        printf("%d\n", array[i]); //gets the indexes and numbers and prints out
     }
     
-    printf("And your total elements are: %d", element);
-
-    return EXIT_SUCCESS;
+    printf("And your total elements are: %d\n", element);
+    return 0;
 }
