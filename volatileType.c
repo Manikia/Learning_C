38476454a8c85ab6e8e9ab5139740f22811b048c
Monika <monikaangel.ma15@gmail.com>
2021-06-19 20:14:25 -0700

    learning bit

---
+++ volatileType.c
@@ -0,0 +1,12 @@
+//tells variable that it will change its value
+//restrict is an optimixzationhint for compiler
+//without restriction it asssumes worst case
+
+#include <stdio.h>
+
+int main()
+{
+    volatile int loc1; //volatile value
+
+    return 0;
+}
