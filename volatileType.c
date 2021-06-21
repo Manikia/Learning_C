bf05315aa8af3e343fe2d7222a452afb9e4faed8
Monika <monikaangel.ma15@gmail.com>
2021-06-21 16:32:11 -0700

    learning more c

--- volatileType.c
+++ volatileType.c
@@ -7,6 +7,13 @@
 int main()
 {
     volatile int loc1; //volatile value
-
+    //goto someLabel; //goesto the statement that has the label of 'somelabel'
+    //goto isnt very liked
+    //ex:
+    //goto jump
+    //jump:
+        //statement with what i want it to do
+    //the above work will go from the goto to jump since thats
+    //where im telling it to goto**
     return 0;
 }
